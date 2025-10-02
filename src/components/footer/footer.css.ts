105f9c524451e5a39e631484e1cf997245bad17d
Mate Horvath <mahte007@gmail.com>
2025-10-02 17:36:20 +0200

    responsive footer

--- src/components/footer/footer.css.ts
+++ src/components/footer/footer.css.ts
@@ -11,7 +11,14 @@ export const mainContainer = style({
     display: "flex",
     justifyContent: "space-between",
     marginInline: "128px",
-    padding: "32px"
+    padding: "32px",
+    "@media": {
+    "screen and (max-width: 675px)": {
+      flexDirection: "column",
+      alignItems: "center",
+      gap: "16px"
+    },
+  },
 })
 
 export const bottomLine = style({
@@ -19,4 +26,14 @@ export const bottomLine = style({
     width: '100%',
     background: 'linear-gradient(to right, transparent, #e0e0e0, transparent)',
     marginTop: '8px',
+})
+
+export const linkContainer = style({
+    display: "flex",
+    gap: "16px",
+    "@media": {
+    "screen and (max-width: 675px)": {
+      flexDirection: "column",
+    },
+  },
 })
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,9 +8,9 @@ export default function Footer() {
             <div className={styles.bottomLine} />
             <div className={styles.mainContainer}>
               <div>© {new Date().getFullYear()} Máté Horváth</div>
-              <div className="flex gap-4 mt-3 sm:mt-0">
-                <a href="#" aria-label="GitHub" className="flex items-center gap-2"> <Github size={16} /> GitHub</a>
-                <a href="#" aria-label="LinkedIn" className="flex items-center gap-2"> <Linkedin size={16} /> LinkedIn</a>
+              <div className={styles.linkContainer}>
+                <a href="https://github.com/mahte007" target="_blank" aria-label="GitHub" className="flex items-center gap-2"> <Github size={16} /> GitHub</a>
+                <a href="https://www.linkedin.com/in/horvath-mate3/" target="_blank" aria-label="LinkedIn" className="flex items-center gap-2"> <Linkedin size={16} /> LinkedIn</a>
               </div>
             </div>
         </footer>
