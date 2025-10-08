e24db6f9bba63e83b9081e30e2dd2ac0c75fe1ab
Mate Horvath <mahte007@gmail.com>
2025-10-08 21:32:47 +0200

    fixed orbs for mobile and bigger page height

--- src/components/common/orbs/orbs.tsx
+++ src/components/common/orbs/orbs.tsx
@@ -11,7 +11,7 @@ type OrbConfig = {
 
 const OrbBackground: React.FC<{ pageHeight?: number }> = ({ pageHeight }) => {
   const orbs: OrbConfig[] = [
-    { size: 100, color: '#ff6b6b', top: '10%', left: '15%', duration: '25s' },
+    { size: 100, color: '#ff6b6b', top: '5%', left: '15%', duration: '25s' },
     { size: 200, color: '#6bc1ff', top: '25%', left: '65%', duration: '30s' },
     { size: 80,  color: '#ffdd6b', top: '10%', left: '45%', duration: '20s' },
     { size: 120, color: '#9bff6b', top: '30%', left: '10%', duration: '28s' },
@@ -20,13 +20,15 @@ const OrbBackground: React.FC<{ pageHeight?: number }> = ({ pageHeight }) => {
     { size: 100, color: '#ff6b6b', top: '60%', left: '40%', duration: '25s' },
     { size: 150, color: '#6bc1ff', top: '65%', left: '70%', duration: '30s' },
     { size: 80, color: '#ffdd6b', top: '80%', left: '20%', duration: '20s' },
-    { size: 120, color: '#9bff6b', top: '85%', left: '60%', duration: '28s' },
+    { size: 120, color: '#6bffa1', top: '85%', left: '70%', duration: '30s' },
+    { size: 90, color: '#b76bff', top: '90%', left: '90%', duration: '25s' },
+    { size: 60, color: '#9bff6b', top: '90%', left: '20%', duration: '28s' },
   ];
 
   return (
     <div
       className={orbWrapper}
-      style={{ height: `${pageHeight}px` }} // spreads across full scroll height
+      style={{ height: `${pageHeight && pageHeight + 500}px` }} // spreads across full scroll height
     >
       {orbs.map((o, index) => (
         <div
